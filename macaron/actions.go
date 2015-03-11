57808118cd7e015e5391d935d01d01a2ae3c839b
Dustin Willis Webber <dustin.webber@gmail.com>
2015-03-11 14:35:44 -0400

    add basic new project logic

--- macaron/actions.go
+++ macaron/actions.go
@@ -1,9 +1,37 @@
 package main
 
-import "github.com/codegangsta/cli"
+import (
+	"os"
+
+	"github.com/Unknwon/com"
+	"github.com/codegangsta/cli"
+)
 
 func NewMacaronProject(cli *cli.Context) {
+
+	args := cli.Args()
+
+	if len(args) < 2 {
+		// error - need project name and location
+		os.Exit(1)
+	}
+
+	name := args.Get(0)
+	location := args.Get(1)
+
+	if !com.IsExist(location) {
+		// error - not a path..
+		os.Exit(1)
+	}
+
 	// macaron new project_name
+	project := Project{
+		Name:     name,
+		Location: location,
+	}
+
+	project.Init()
+	project.Build()
 }
 
 func NewMacaronScaffold(cli *cli.Context) {
---
+++ macaron/project.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/BurntSushi/toml"
+)
+
+type Project struct {
+	Name     string
+	Location string
+	Config   ProjectConfig
+}
+
+type ProjectConfig struct {
+	Build  build
+	Readme readme
+	Git    git
+}
+
+type build struct {
+	Directories []string
+}
+
+type readme struct {
+	Enabled bool
+	Format  string
+}
+
+type git struct {
+	Enabled bool
+}
+
+func (self *Project) Init() {
+	if _, err := toml.DecodeFile("templates/new/config.toml", &self.Config); err != nil {
+		// handle error
+	}
+
+	fmt.Println(self.Config)
+}
+
+func (self *Project) Build() {
+}
