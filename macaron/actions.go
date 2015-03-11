f63e3d22e20ce4a6efd4d72dd983e0808f42c4d4
Dustin Willis Webber <dustin.webber@gmail.com>
2015-03-11 14:57:44 -0400

    update location logic

--- macaron/actions.go
+++ macaron/actions.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/Unknwon/com"
 	"github.com/codegangsta/cli"
@@ -17,10 +19,20 @@ func NewMacaronProject(cli *cli.Context) {
 	}
 
 	name := args.Get(0)
-	location := args.Get(1)
+
+	location, err := filepath.Abs(args.Get(1))
+
+	if err != nil {
+		os.Exit(1)
+	}
 
 	if !com.IsExist(location) {
 		// error - not a path..
+		os.Exit(1)
+	}
+
+	if err := PathInsideGoPath(location); err != nil {
+		// show error - you need to be inside your $GOPATH
 		os.Exit(1)
 	}
 
@@ -30,8 +42,13 @@ func NewMacaronProject(cli *cli.Context) {
 		Location: location,
 	}
 
+	fmt.Println("name:", name)
+	fmt.Println("location:", location)
+
 	project.Init()
 	project.Build()
+
+	fmt.Println("working")
 }
 
 func NewMacaronScaffold(cli *cli.Context) {
